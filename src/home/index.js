c600b82d076f4b48285ac904e9bb81e7d33426e6
Ludwig <hg1483@nyu.edu>
2019-07-09 20:21:30 +0800

    antd and babel-plugin

--- src/home/index.js
+++ src/home/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-
+import { Button } from 'antd'
 class Home extends Component {
 	render() {
 		return (
-			<div className="home">
+			<div>
+				<div className="home">
 				home
 			</div>
+			<Button>This is a test of Antd </Button>
+			</div>
+			
 		);
 	}
 }
