7439a2ac466c42b9117c30db3e31eefd4adac81f
Ludwig <hg1483@nyu.edu>
2019-07-10 19:57:52 +0800

    start

--- src/router.js
+++ src/router.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Start from './start.js'
 import {
   routerRedux,
   Route,
-  Switch,
-  HashRouter
 } from 'dva/router';
 import './index.less'
-import Start from './start.js'
 const { ConnectedRouter } = routerRedux;
 export default function Router({ history }) {
   return (
-    <ConnectedRouter history={history}>
-      <HashRouter>
-        <Switch>
-          <Route path='/' component={Start} />
-        </Switch>
-      </HashRouter>
-    </ConnectedRouter>
+        <ConnectedRouter history={history}>
+          <Route path='/' component={Start}></Route>
+        </ConnectedRouter>
   );
 }
 
